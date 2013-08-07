e0f494bd28554040788039a5e98258ae6cdd90fb
q45 <quintinsmith@icloud.com>
2013-08-07 13:46:57 -0600

    added operator to print out based on correct choice

--- calc.rb
+++ calc.rb
@@ -9,19 +9,23 @@ say("What the second number")
 num2 = gets.chomp
 
 say "What do you want to do 1) add 2) subtract 3) multiply 4) divide"
-operator = gets.chomp
+operator = gets.chomp.to_s
 
 if operator == "1"
 	result = num1.to_i + num2.to_i
+	operator = "+"
 elsif operator == "2"
 	result = num1.to_i - num2.to_i
+	operator = "-"
 elsif operator == "3"
 	result = num1.to_i * num2.to_i
+	operator = "*"
 elsif operator == "4"
 	result = num1.to_f / num2.to_f
+	operator = "/"
 else
 	puts "Please pick a number 1-4"
 	
 end
 
-say " #{num1} + #{num2} = #{result}"
+say " #{num1} #{operator} #{num2} = #{result}"
