f8e5629ddaa440e1e28f9a8b8da43d4aa9a4e1e7
q45 <quintinsmith@icloud.com>
2013-08-07 13:31:30 -0600

    calc program in ruby

---
+++ calc.rb
@@ -0,0 +1,27 @@
+def say(msg)
+	puts "=> #{msg}"
+end
+
+say("Whats the first number")
+num1 = gets.chomp
+
+say("What the second number")
+num2 = gets.chomp
+
+say "What do you want to do 1) add 2) subtract 3) multiply 4) divide"
+operator = gets.chomp
+
+if operator == "1"
+	result = num1.to_i + num2.to_i
+elsif operator == "2"
+	result = num1.to_i - num2.to_i
+elsif operator == "3"
+	result = num1.to_i * num2.to_i
+elsif operator == "4"
+	result = num1.to_f / num2.to_f
+else
+	puts "Please pick a number 1-4"
+	
+end
+
+say " #{num1} + #{num2} = #{result}"
